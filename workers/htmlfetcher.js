c6220260eebb38c911bd35487658a06f149a109c
comesm <comesm@gmail.com>
2017-01-12 17:26:53 -0800

    implement archiving sites with data

--- workers/htmlfetcher.js
+++ workers/htmlfetcher.js
@@ -1,6 +1,15 @@
 var archive = require('../helpers/archive-helpers');
+var CronJob = require('cron').CronJob;
+var job = new CronJob('* * * * * *', function() {
+  archive.readListOfUrls(function(err, data) {
+    if (err) {
+      console.log('line 6', err);
+    } else {
+      archive.downloadUrls(data.slice(0, data.length - 1));
+    }   
+  });
+}, null, true, 'America/Los_Angeles');
+job.start();
+
 // Use the code in `archive-helpers.js` to actually download the urls
 // that are waiting.
-
-// create urlArray from sites.txt
-archive.downloadUrls(urlArray);
